f03945d76b4165b133e19578811ac655015334f0
student <student@Students-MacBook-Pro-3.local>
2016-12-22 10:34:38 -0500

    altered structure; links not yet in place

---
+++ data.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: student
+ * Date: 12/22/16
+ * Time: 10:27 AM
+ */
+
+$link_home = 'index.php';
+$link_about = 'about/';
+$link_publications = 'publications/';
+$link_people = 'people/';
+$link_contact = 'contact/';
+
+$link_root = '../';
+
+$style_root = 'stylesheets/';
+$style_master = 'master.css';
+
+$style_home = 'home.css';
+$style_about = $link_root.$style_root.'about.css';
+$style_ = $link_root.$style_root.'about.css';
